8914f33eedcef171244417b6d636ef430dd9c8ab
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-05-11 20:23:50 +0600

    crud

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -26,12 +26,47 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
+
+    app.get('/services', async(req, res)=>{
+        const carsor = servicesCollection.find();
+        const result = await carsor.toArray(carsor);
+        res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await servicesCollection.findOne(query)
+        res.send(result);
+    })
+
+    //bookings
+    app.post('/bookings', async (req,res)=>{
+        const booking= req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result)
+    })
+
+    app.get('/bookings', async(req, res)=>{ 
+        console.log(req.query);
+        let query= {};
+        if(req.query?.email){
+            query= {email: req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
