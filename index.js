d2361185968a9f1b1548401f9e2300be14e6fbb3
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-05-15 14:51:39 +0600

    added patch

--- index.js
+++ index.js
@@ -61,6 +61,29 @@ async function run() {
         res.send(result);
     })
 
+    app.patch('/bookings/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const updateBookings = req.body;
+      console.log(updateBookings);
+      const updateDoc ={
+        $set:{
+          status:updateBookings.status
+        }
+      }
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result)
+
+    })
+    
+    app.delete('/bookings/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result);
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
