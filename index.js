abad6da1695f04239f419afae453203be41df206
mehedi-hasan16 <hasanmehedi6541@gmail.com>
2023-05-15 18:35:12 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -22,6 +23,26 @@ const client = new MongoClient(uri, {
   }
 });
 
+//vertify jwt token 
+const verifyJWT = (req, res, next) => {
+  console.log('hitting verify jwt');
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "unauthorized access" })
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+    if(error){
+      return res.status(401).send({error:true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,57 +51,75 @@ async function run() {
     const servicesCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
-    app.get('/services', async(req, res)=>{
-        const carsor = servicesCollection.find();
-        const result = await carsor.toArray(carsor);
-        res.send(result);
+    //jwt 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
+      res.send({ token })
     })
 
-    app.get('/services/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id : new ObjectId(id)};
-        const result = await servicesCollection.findOne(query)
-        res.send(result);
+    app.get('/services', async (req, res) => {
+      const carsor = servicesCollection.find();
+      const result = await carsor.toArray(carsor);
+      res.send(result);
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query)
+      res.send(result);
     })
 
     //bookings
-    app.post('/bookings', async (req,res)=>{
-        const booking= req.body;
-        console.log(booking);
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result)
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result)
     })
 
-    app.get('/bookings', async(req, res)=>{ 
-        console.log(req.query);
-        let query= {};
-        if(req.query?.email){
-            query= {email: req.query.email}
-        }
-        const result = await bookingCollection.find(query).toArray();
-        res.send(result);
+    app.get('/bookings', verifyJWT, async (req, res) => {
+
+      // console.log(req.headers.authorization);
+      const decoded = req.decoded;
+
+
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error: 1, message: 'forbidden access'});
+      }
+
+
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
     })
 
-    app.patch('/bookings/:id', async(req, res)=>{
+    app.patch('/bookings/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updateBookings = req.body;
       console.log(updateBookings);
-      const updateDoc ={
-        $set:{
-          status:updateBookings.status
+      const updateDoc = {
+        $set: {
+          status: updateBookings.status
         }
       }
       const result = await bookingCollection.updateOne(filter, updateDoc);
       res.send(result)
 
     })
-    
-    app.delete('/bookings/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query ={_id: new ObjectId(id)}
-        const result = await bookingCollection.deleteOne(query);
-        res.send(result);
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
 
     })
 
@@ -96,11 +135,11 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('car doctor server is running')
+app.get('/', (req, res) => {
+  res.send('car doctor server is running')
 })
 
 
-app.listen(port, ()=>{
-    console.log(`car doctor server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`car doctor server is running on port ${port}`);
 })
